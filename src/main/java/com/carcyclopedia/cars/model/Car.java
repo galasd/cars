5719a4ac583456004a59abb70d033e483755960a
daveg <dave.galas@gmail.com>
2020-08-29 21:58:43 +0200

    Model and repository

---
+++ src/main/java/com/carcyclopedia/cars/model/Car.java
@@ -0,0 +1,58 @@
+package com.carcyclopedia.cars.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "manufacturer")
+    private String manufacturer;
+
+    @Column(name = "model")
+    private String model;
+
+    @Column(name = "year")
+    private int year;
+
+    public Car(){
+
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public String getManufacturer(){
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer){
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel(){
+        return model;
+    }
+
+    public void setModel(String model){
+        this.model = model;
+    }
+
+    public int getYear(){
+        return year;
+    }
+
+    public void setYear(int year){
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Car [id=" + id + ", manufacturer=" + manufacturer + ", model=" + model + ", year=" + year + "]";
+    }
+}
