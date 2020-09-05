fee8e766d949c58ed435fd1dcbf6d26be99b96c4
daveg <dave.galas@gmail.com>
2020-09-05 12:01:24 +0200

    Sorting edit

--- src/main/java/com/carcyclopedia/cars/repository/CarRepository.java
+++ src/main/java/com/carcyclopedia/cars/repository/CarRepository.java
@@ -3,11 +3,9 @@ package com.carcyclopedia.cars.repository;
 import com.carcyclopedia.cars.model.Car;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
 
-    public Page<Car> findByManufacturer(String manufacturer, Pageable pageable);
-    public Page<Car> findByManufacturer(String manufacturer, Sort sort);
+    Page<Car> findByManufacturer(String manufacturer, Pageable pageable);
 }
