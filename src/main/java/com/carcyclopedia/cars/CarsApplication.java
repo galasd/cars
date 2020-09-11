943d2e66f6875b755207556cdc0eda1225c632ae
daveg <dave.galas@gmail.com>
2020-09-11 13:06:02 +0200

    Logging using aspects added

--- src/main/java/com/carcyclopedia/cars/CarsApplication.java
+++ src/main/java/com/carcyclopedia/cars/CarsApplication.java
@@ -2,12 +2,13 @@ package com.carcyclopedia.cars;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 @SpringBootApplication
 public class CarsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarsApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/carcyclopedia/cars/aspect/CarsViewAspect.java
@@ -0,0 +1,33 @@
+package com.carcyclopedia.cars.aspect;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class CarsViewAspect {
+
+    private Logger log = LogManager.getLogger(this.getClass());
+
+    /**
+     * Pointcut that matches all repositories, services and Web REST endpoints.
+     */
+    @Pointcut("within(@org.springframework.stereotype.Repository *)" +
+            " || within(@org.springframework.stereotype.Service *)" +
+            " || within(@org.springframework.web.bind.annotation.RestController *)")
+    public void springBeanPointcut() {
+        // Method is empty as this is just a Pointcut, the implementations are in the advices.
+    }
+
+    @AfterThrowing(pointcut = "springBeanPointcut()", throwing = "e")
+    public void logAfterThrowing(JoinPoint joinPoint, Throwable e) {
+       log.error("Exception in {}.{}() with cause = {}.", joinPoint.getSignature().getDeclaringTypeName(),
+               joinPoint.getSignature().getName(), e.getCause() != null ? e.getCause() : "NULL");
+    }
+
+}
