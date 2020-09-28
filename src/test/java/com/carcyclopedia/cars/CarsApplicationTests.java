1f49512b420bd8b0e2607f7f20e88cfdc96ddf18
daveg <dave.galas@gmail.com>
2020-09-28 12:59:31 +0200

    Car testing edit

--- src/test/java/com/carcyclopedia/cars/CarsApplicationTests.java
+++ src/test/java/com/carcyclopedia/cars/CarsApplicationTests.java
@@ -2,8 +2,10 @@ package com.carcyclopedia.cars;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations="classpath:application-dev.properties")
 class CarsApplicationTests {
 
 	@Test
